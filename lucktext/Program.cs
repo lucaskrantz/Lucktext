96431559b13db1d93f1eda812ac41f29ec65b63b
Lucas Krantz <lucas.krantz@elev.ga.ntig.se>
2022-08-31 14:24:22 +0200

    Initial commit

---
+++ lucktext/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+Random generator = new Random();
+
+Console.WriteLine("Gissa på ett nummer mellan 1 - 10");
+
+int rand = generator.Next(1, 11);
+int gissning = -1;
+
+while (!(gissning > 0 && gissning < 11))
+{
+    try
+    {
+        gissning = Convert.ToInt32(Console.ReadLine());
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine("Ett fel inträffade. Ange ett positivt heltal!");
+    }
+}
+
+if (gissning == rand)
+{
+    Console.WriteLine("Grattis, du gissade rätt");
+    Console.ReadLine();
+}
+else
+{
+    Console.WriteLine($"Du gissade fel, bättre lycka nästa gång! Rätt nummer var {rand}");
+    Console.ReadLine();
+}
+
